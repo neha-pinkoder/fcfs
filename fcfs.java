07c79db463b6fa72eab36e24ee6ac1dae8b269a7
Neha <nehagholap0307@gmail.com>
2022-12-14 18:40:26 +0530

    fcfs working fine

---
+++ fcfs.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+/**
+ * Created by Neha Gholap
+ * Roll No. 222011014
+ */
+public class fcfs {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number of process");
+        Scanner in = new Scanner(System.in);
+        int numberOfProcesses = in.nextInt();
+
+        int pid[] = new int[numberOfProcesses];
+        int bt[] = new int[numberOfProcesses]; // burst time
+        int ar[] = new int[numberOfProcesses]; // arrival time
+        int ct[] = new int[numberOfProcesses]; // completion time
+        int ta[] = new int[numberOfProcesses]; // turnaround time
+        int wt[] = new int[numberOfProcesses]; // waiting time
+
+        for (int i = 0; i < numberOfProcesses; i++) {
+            System.out.println("Enter process " + (i + 1) + " arrival time: ");
+            ar[i] = in.nextInt();
+            System.out.println("Enter process " + (i + 1) + " burst time: ");
+            bt[i] = in.nextInt();
+            pid[i] = i + 1;
+        }
+        int temp;
+        for (int i = 0; i < numberOfProcesses; i++) {
+            for (int j = i + 1; j < numberOfProcesses; j++) {
+
+                if (ar[i] > ar[j]) {
+                    temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+
+                    temp = pid[i];
+                    pid[i] = pid[j];
+                    pid[j] = temp;
+                    temp = bt[i];
+                    bt[i] = bt[j];
+                    bt[j] = temp;
+                }
+            }
+        }
+
+        System.out.println();
+        ct[0] = bt[0] + ar[0];
+        for (int i = 1; i < numberOfProcesses; i++) {
+            ct[i] = ct[i - 1] + bt[i];
+        }
+        for (int i = 0; i < numberOfProcesses; i++) {
+            ta[i] = ct[i] - ar[i];
+            wt[i] = ta[i] - bt[i];
+        }
+        System.out.println("Process\t\tAT\t\tBT\t\tCT\t\tTAT\t\tWT");
+        for (int i = 0; i < numberOfProcesses; i++) {
+            System.out.println(
+                    "P" + pid[i] + "\t\t" + ar[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t" + ta[i] + "\t\t" + wt[i]);
+        }
+
+        System.out.println("gantt chart: ");
+        for (int i = 0; i < numberOfProcesses; i++) {
+            System.out.print("P" + pid[i] + " ");
+        }
+        double totalWaitingTime = 0, totalTurnaroundTime = 0;
+        for (int i = 0; i < numberOfProcesses; i++) {
+            totalTurnaroundTime += ta[i];
+            totalWaitingTime += wt[i];
+        }
+        System.out.println("Average TurnAround Time = " + (totalTurnaroundTime / numberOfProcesses));
+        System.out.println("Average Waiting Time = " + (totalWaitingTime / numberOfProcesses));
+    }
+
+}
